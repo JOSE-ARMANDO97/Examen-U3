74a8e82106657a4e95ed89c9c98a6b616cd45230
JOSE-ARMANDO97 <71796689+JOSE-ARMANDO97@users.noreply.github.com>
2020-11-22 23:22:46 -0600

    Create NodoVIP.java

---
+++ NodoVip/NodoVIP.java
@@ -0,0 +1,16 @@
+package NodoVip;
+
+public class NodoVIP {
+    String Clientes;
+      public int elemento;
+    public NodoVIP siguiente;
+    
+
+    public NodoVIP(String Clientes, int elemento) {
+        this.Clientes=Clientes;
+        this.elemento=elemento;
+      siguiente=null;
+    }
+
+   
+}
