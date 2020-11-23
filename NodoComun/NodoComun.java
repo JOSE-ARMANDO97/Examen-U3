65ce99fbef088b89dda4b0d179c4e1542f9d6b36
JOSE-ARMANDO97 <71796689+JOSE-ARMANDO97@users.noreply.github.com>
2020-11-22 23:21:07 -0600

    Create NodoComun.java

---
+++ NodoComun/NodoComun.java
@@ -0,0 +1,13 @@
+package NodoComun;
+
+public class NodoComun{
+public int elemento;
+    public NodoComun siguiente;
+    String Clientes;
+    public NodoComun(String Clientes,int elemento) {
+        this.Clientes=Clientes;
+      this.elemento=elemento;
+      siguiente=null;
+    }
+}
+
